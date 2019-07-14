f50f5141ea760fc49ffc060b561eefd3b1a7e8d3
silvioravaioli <silvio.ravaioli@gmail.com>
2019-07-14 00:03:04 -0400

    update

--- DDM/database_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//$dbc = mysql_connect('mysql8.000webhost.com', 'a9458604_decisio', 'silvio');
-//mysql_select_db('a9458604_decisio', $dbc);
-
-$dbc = mysql_connect('127.0.0.1', 'root', '');
-mysql_select_db('decison', $dbc);
-
-?>
